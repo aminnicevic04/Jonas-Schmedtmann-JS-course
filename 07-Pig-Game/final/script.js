088e27b5cd61080bdc62f63e940ee2a0fee79af6
aminnicevic04 <aminnicevic@gmail.com>
2023-01-05 00:17:50 +0100

    From Zero to Expert

--- 07-Pig-Game/final/script.js
+++ 07-Pig-Game/final/script.js
@@ -13,7 +13,7 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
-// let scores, currentScore, activePlayer, playing;
+let scores, currentScore, activePlayer, playing;
 
 // Starting conditions
 const init = function () {
