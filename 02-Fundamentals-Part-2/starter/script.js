b958c07346d7641466af2f483d3fc7a6c07e53e1
aminnicevic04 <aminnicevic@gmail.com>
2023-01-08 19:34:39 +0100

    a

---
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let hasDriverLicense = false;
+const passTest = true;
+
+if (passTest) hasDriverLicense = true;
+if (hasDriverLicense) console.log("i can drive");
+
+// const interface = "audio";
+// const private = 534;
+
+// 2
