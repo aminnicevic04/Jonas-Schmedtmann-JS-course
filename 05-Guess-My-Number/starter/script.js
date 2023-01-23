10167aa85bf8294b61e08a1e83a5ad93bb2dc67e
aminnicevic04 <aminnicevic@gmail.com>
2023-01-23 23:00:04 +0100

    a

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,5 @@
 'use strict';
+// DOM-Document Object Model
+// API-Aplication Programming Interface
+console.log(document.querySelector('.message').textContent);
+//prikazivanje selektovanog texta u console
