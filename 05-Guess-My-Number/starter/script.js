77a481c36eb7246da9038fa47478129f0451ce0e
aminnicevic04 <aminnicevic@gmail.com>
2023-01-24 18:59:32 +0100

    From Zero To Hero

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -20,18 +20,26 @@ const x = function () {
 */
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
+let score = 5;
 
-document.querySelector('.number').textContent = secretNumber;
+// document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value); //prebacujemo string u number
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = '⛔ No number!';
+
+    //when player win
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('body').style.backgroundColor = '#14c91a';
+    document.querySelector('.number').style.width = '30rem';
+
+    //when guess is too high
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📈 Too high!';
@@ -40,15 +48,50 @@ document.querySelector('.check').addEventListener('click', function () {
     } else {
       document.querySelector('.message').textContent = '💥 You lost the game!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red'; //radi
+      document.querySelector('.between').style.color = 'white'; //radi
+      document.querySelector('h1').style.color = 'white'; //radi
+      document.querySelector('header').style.borderBottom = '7px solid white'; //radi
+      document.querySelector('.number').style.color = 'red'; //radi
+      document.querySelector('.number').style.backgroundColor = 'white'; //radi
+      document.querySelector('.number').style.width = '30rem'; //radi
+      document.querySelector('.message').style.color = 'white'; //radi
+      document.querySelector('.label-score').style.color = 'white'; //radi
+      document.querySelector('.label-highscore').style.color = 'white'; //radi
+      document.querySelector('.guess').style.border = '4px solid white'; //radi
+      document.querySelector('.guess').style.color = 'white'; //radi
+      document.querySelector('button').style.color = 'red'; //radi
+      document.querySelector('button').style.backgroundColor = 'white'; //radi
+      document.querySelector('button.check').style.color = 'red'; //radi
+      document.querySelector('button.check').style.backgroundColor = 'white'; //radi
     }
+
+    //when guess is too low
   } else if (guess < secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = '📉 Too low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.message').textContent =
+        '💥 You lost the game!!!';
       document.querySelector('.score').textContent = 0;
+      document.querySelector('body').style.backgroundColor = 'red'; //radi
+      document.querySelector('.between').style.color = 'white'; //radi
+      document.querySelector('h1').style.color = 'white'; //radi
+      document.querySelector('header').style.borderBottom = '7px solid white'; //radi
+      document.querySelector('.number').style.color = 'red'; //radi
+      document.querySelector('.number').style.backgroundColor = 'white'; //radi
+      document.querySelector('.number').style.width = '30rem'; //radi
+      document.querySelector('.message').style.color = 'white'; //radi
+      document.querySelector('.label-score').style.color = 'white'; //radi
+      document.querySelector('.label-highscore').style.color = 'white'; //radi
+      document.querySelector('.guess').style.border = '4px solid white'; //radi
+      document.querySelector('.guess').style.color = 'white'; //radi
+      document.querySelector('button').style.color = 'red'; //radi
+      document.querySelector('button').style.backgroundColor = 'white'; //radi
+      document.querySelector('button.check').style.color = 'red'; //radi
+      document.querySelector('button.check').style.backgroundColor = 'white'; //radi
     }
     // document.querySelector('.message').textContent = '📉 Too low!';
     // score--; //smanjivanje skora kad pogresite
