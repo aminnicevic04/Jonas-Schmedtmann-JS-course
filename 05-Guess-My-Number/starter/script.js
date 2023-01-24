13236d39ec378ba37436fe50072552e7347cca11
aminnicevic04 <aminnicevic@gmail.com>
2023-01-24 01:22:28 +0100

    a

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,5 +1,59 @@
 'use strict';
+
+/*
 // DOM-Document Object Model
 // API-Aplication Programming Interface
 console.log(document.querySelector('.message').textContent);
 //prikazivanje selektovanog texta u console
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
+
+const x = function () {
+    console.log(32);
+};
+*/
+
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+
+document.querySelector('.number').textContent = secretNumber;
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value); //prebacujemo string u number
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = '🎉 Correct Number!';
+  } else if (guess > secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📈 Too high!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game!';
+      document.querySelector('.score').textContent = 0;
+    }
+  } else if (guess < secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '📉 Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥 You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
+    // document.querySelector('.message').textContent = '📉 Too low!';
+    // score--; //smanjivanje skora kad pogresite
+    // document.querySelector('.score').textContent = score; //ispisivanje smanjenog skora
+  }
+  //   document.querySelector('.message').textContent = '🎉 Correct Number!';
+  //menja se massage
+});
