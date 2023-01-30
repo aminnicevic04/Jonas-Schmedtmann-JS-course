fe47e80e5245cf5f4b37e65891e4f8b066bd4bdc
aminnicevic04 <aminnicevic@gmail.com>
2023-01-30 19:40:51 +0100

    a

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -21,6 +21,7 @@ diceEl.classList.add('hidden');
 const scores = [0, 0]; //sadrzi skorove od player1 i player 2
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function () {
   currentScore = 0;
@@ -32,53 +33,59 @@ const switchPlayer = function () {
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
-  // 1.generating a random dice roll
+  if (playing) {
+    // 1.generating a random dice roll
 
-  const dice = Math.trunc(Math.random() * 6) + 1;
-  //ovo je localna varijabla i ne mozemo je koristiti izvan funckije jer nije globalna
-  //math.random da dobijemo random broj do 6
-  // +1 koristimo zbog toga sto program broji 0,1,2,3,4,5. i mi dodajemo jos +1
-  //math.trunc da ne dobijamo decimalne brojeve
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    //ovo je localna varijabla i ne mozemo je koristiti izvan funckije jer nije globalna
+    //math.random da dobijemo random broj do 6
+    // +1 koristimo zbog toga sto program broji 0,1,2,3,4,5. i mi dodajemo jos +1
+    //math.trunc da ne dobijamo decimalne brojeve
 
-  // 2.display dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${dice}.png`;
-  //uzimamo source od slike i na mesto gde je broj stavljamo dice
+    // 2.display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+    //uzimamo source od slike i na mesto gde je broj stavljamo dice
 
-  // 3.chcek for rolled 1: if true switch to next player
-  if (dice !== 1) {
-    // Add dice to the current score
-    currentScore = currentScore + dice;
-    document.getElementById(`current--${activePlayer}`).textContent =
-      currentScore; //nas id je dinamican i menja se good trick btw
-  } else {
-    // Switch to next player
-    switchPlayer();
-    //toggle method za dodavanje ove klase kada je potrebna
-    //koji zapravo menja pozadinu i vidi se kada se promeni player
-    //Metoda toggle() uklanja postojeći token sa liste i vraća false.
-    //Ako token ne postoji, dodaje se i funkcija vraća true.
+    // 3.chcek for rolled 1: if true switch to next player
+    if (dice !== 1) {
+      // Add dice to the current score
+      currentScore = currentScore + dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore; //nas id je dinamican i menja se good trick btw
+    } else {
+      // Switch to next player
+      switchPlayer();
+      //toggle method za dodavanje ove klase kada je potrebna
+      //koji zapravo menja pozadinu i vidi se kada se promeni player
+      //Metoda toggle() uklanja postojeći token sa liste i vraća false.
+      //Ako token ne postoji, dodaje se i funkcija vraća true.
+    }
   }
 });
 
 btnHold.addEventListener('click', function () {
-  // 1. Add current score to active players score
-  scores[activePlayer] += currentScore;
-  // scores[1] = scores[1] + currentScore;
-  document.getElementById(`score--${activePlayer}`).textContent =
-    scores[activePlayer];
+  if (playing) {
+    // 1. Add current score to active players score
+    scores[activePlayer] += currentScore;
+    // scores[1] = scores[1] + currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
 
-  // 2. Check if players score is >= 100
-  if (scores[activePlayer] >= 100) {
-    // Finish Game
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.add('player-winner');
-    document
-      .querySelector(`.player--${activePlayer}`)
-      .classList.remove('player-active');
-  } else {
-    // Switch to the next player
-    switchPlayer();
+    // 2. Check if players score is >= 100
+    if (scores[activePlayer] >= 20) {
+      // Finish Game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      // Switch to the next player
+      switchPlayer();
+    }
   }
 });
