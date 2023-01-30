38f781a7201bafbdabc5a4a560c130e89baec523
aminnicevic04 <aminnicevic@gmail.com>
2023-01-30 19:16:21 +0100

    a

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -18,9 +18,17 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+const scores = [0, 0]; //sadrzi skorove od player1 i player 2
 let currentScore = 0;
 let activePlayer = 0;
+
+const switchPlayer = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player1El.classList.toggle('player--active');
+  player0El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -45,14 +53,32 @@ btnRoll.addEventListener('click', function () {
       currentScore; //nas id je dinamican i menja se good trick btw
   } else {
     // Switch to next player
-    currentScore = 0;
-    document.getElementById(`current--${activePlayer}`).textContent = 0;
-    activePlayer = activePlayer === 0 ? 1 : 0;
-    player1El.classList.toggle('player--active');
-    player0El.classList.toggle('player--active');
+    switchPlayer();
     //toggle method za dodavanje ove klase kada je potrebna
     //koji zapravo menja pozadinu i vidi se kada se promeni player
     //Metoda toggle() uklanja postojeći token sa liste i vraća false.
     //Ako token ne postoji, dodaje se i funkcija vraća true.
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  // 1. Add current score to active players score
+  scores[activePlayer] += currentScore;
+  // scores[1] = scores[1] + currentScore;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    scores[activePlayer];
+
+  // 2. Check if players score is >= 100
+  if (scores[activePlayer] >= 100) {
+    // Finish Game
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player-winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player-active');
+  } else {
+    // Switch to the next player
+    switchPlayer();
   }
 });
