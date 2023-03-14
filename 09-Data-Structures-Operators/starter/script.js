ece6167e3ee5aba9e01196a077db622f96b2c106
aminnicevic04 <aminnicevic@gmail.com>
2023-03-14 17:54:56 +0100

    a

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -810,37 +810,292 @@ for (const player of game.scored) {
 }
 */
 
-const rest = new Map();
-rest.set('name', 'clasico Italiano');
-rest.set(1, 'Firenze, Italy');
-rest.set(2, 'Listabon, Portugal');
-console.log(rest.set(2, 'Listabon, Portugal'));
+// const rest = new Map();
+// rest.set('name', 'clasico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// rest.set(2, 'Listabon, Portugal');
+// console.log(rest.set(2, 'Listabon, Portugal'));
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'we are open')
-  .set(false, 'we are closed');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'we are open')
+//   .set(false, 'we are closed');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 21;
-rest.get(time > rest.get('open') && time < rest.get('close'));
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 21;
+// rest.get(time > rest.get('open') && time < rest.get('close'));
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
-rest.delete(2);
-// rest.clear()
+// console.log(rest.has('categories'));
+// rest.delete(2);
+// // rest.clear()
 
-const arr = [1, 2];
-rest.set(arr, 'test');
-rest.set([document.querySelector('h1'), 'Heading']);
-rest.delete(rest);
-rest.delete(rest.size);
+// const arr = [1, 2];
+// rest.set(arr, 'test');
+// rest.set([document.querySelector('h1'), 'Heading']);
+// rest.delete(rest);
+// rest.delete(rest.size);
 
-console.log(rest.get(arr));
-const amci = 1;
-c;
+// console.log(rest.get(arr));
+
+// maps iteration
+// const question = new Map([
+//   ['question', 'what is best...?'],
+//   [1, 'c'],
+//   [2, 'java'],
+//   [3, 'java script'],
+//   ['correct', 3],
+//   [true, 'correct'],
+//   [false, 'try again'],
+// ]);
+// // console.log(question);
+
+// //convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
+
+// //quiz app
+// console.log(question.get('question'));
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') console.log(`answer ${key}:${value}`);
+// }
+// // const answer = Number(prompt('your answer'));
+// const answer = 3;
+// console.log(answer);
+
+// console.log(question.get(question.get('corect') === answer));
+
+// //convert map to array
+
+// console.log(...question);
+// // console.log(question.entries());
+// console.log(...question.keys());
+// console.log(...question.values());
+
+//which data sructure to use
+/*
+Coding Challenge #3
+Let's continue with our football betting app! This time, we have a map called 
+'gameEvents' (see below) with a log of the events that happened during the 
+game. The values are the events themselves, and the keys are the minutes in which 
+each event happened (a football game has 90 minutes plus some extra time).
+Your tasks:
+1. Create an array 'events' of the different game events that happened (no 
+duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 
+was unfair. So remove this event from the game events log.
+3. Compute and log the following string to the console: "An event happened, on 
+average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over 'gameEvents' and log each element to the console, marking 
+whether it's in the first half or second half (after 45 min) of the game, like this:
+[FIRST HALF] 17: ⚽ GOAL
+GOOD LUCK �
+
+const gameEvents = new Map([
+  [17, '⚽ GOAL'], // 17 predstavlja key, a goal value
+  [36, '� Substitution'],
+  [47, '⚽ GOAL'],
+  [61, '� Substitution'],
+  [64, '� Yellow card'],
+  [69, '� Red card'],
+  [70, '� Substitution'],
+  [72, '� Substitution'],
+  [76, '⚽ GOAL'],
+  [80, '⚽ GOAL'],
+  [92, '� Yellow card'],
+]);
+
+//1
+// console.log(gameEvents.values());
+const events = [...new Set(gameEvents.values())];
+console.log(events);
+//2
+gameEvents.delete(64);
+//3
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+//4
+for (const [min, event] of gameEvents) {
+  const half = min <= 45 ? 'first' : 'second';
+  console.log(`[${half} HALF] ${min} ${event}`);
+}
+
+
+// working with strings 1
+const airline = 'tap air portugal';
+const plane = 'a320';
+// console.log(plane[0]);
+// console.log(plane[1]);
+// console.log(plane[2]);
+// console.log('873'[0]);
+
+// console.log(airline.length);
+// console.log('8737'.length);
+
+// console.log(airline.indexOf('r')); //vraca index gde se nalazi r
+// console.log(airline.lastIndexOf('r')); //vraca index gde se nalazi poslednje r
+// console.log(airline.indexOf('portugal')); //vraca index pocetka reci
+// console.log(airline.indexOf('Portugal')); //vraca -1 jer ne postoji
+
+// console.log(airline.slice(4)); // vraca od idnex 4 do kraja
+// console.log(airline.slice(4, 7)); // vraca od idnex 4 do index 7
+
+// console.log(airline.slice(0, airline.indexOf(' '))); // od nula do prvog space
+// console.log(airline.slice(airline.lastIndexOf(' '))); // od zadnjeg razmaka ovako se vraca poslednja rec
+
+// console.log(airline.slice(-2)); // vraca zadnja dva karaktera u stringu
+// console.log(airline.slice(1, -1)); //vraca sve bez zadnjeg karatkera
+
+// const checkMiddleSeat = function (seat) {
+//   //B and E are middle seats
+//   const s = seat.slice(-1);
+//   if (s === 'B' || s === 'E') console.log('u got middle seat');
+//   else console.log('u got lucky');
+// };
+// checkMiddleSeat('118');
+// checkMiddleSeat('23C');
+// checkMiddleSeat('3E');
+
+// working with strings 2
+
+console.log(airline.toLowerCase());
+console.log('amci'.toUpperCase());
+
+// fix capitalization in name
+const passenger = 'AmcI';
+const passengerLower = passenger.toLowerCase();
+const passengerFirst = passenger[0].toUpperCase() + passengerLower.slice(1);
+console.log(passengerFirst);
+
+// Check email
+const email = 'amci@gmail.com';
+const loginEmail = 'Amci@Gmail.Com \n';
+
+// const lowerEmail = loginEmail.toLowerCase();
+// const trimedEmail = lowerEmail.trim();
+// console.log(trimedEmail);
+
+const normalizedEmail = loginEmail.toLowerCase().trim();
+console.log(normalizedEmail);
+console.log(email === normalizedEmail);
+//metoda trim uklanja prazne karaktere na pocetku i kraju nekog stringa
+
+//replacing
+const priceGB = '288,97frf';
+const priceUS = priceGB.replace('frf', '$'); //samo replace
+console.log(priceUS);
+
+const announcment = 'lorem lorem lorem1 lorem lorem1';
+console.log(announcment.replace('lorem1', 'amci')); //replaze samo prvi
+// console.log(announcment.replace(/lorem1/g, 'amci')); //replace sve / /global
+
+//Boleans
+const planee = 'Airbus A320neo';
+console.log(plane.includes('A320')); //vraca true
+console.log(plane.includes('boing')); //vraca false
+console.log(plane.includes('Air')); //vraca true
+//proverava samo da li ima necega u str
+
+if (plane.startsWith('Airbus') && plane.endsWith('neo')) {
+  console.log('part of the new airplanes family');
+}
+
+//practise exercise
+const checkBaggage = function (items) {
+  const baggage = items.toLowerCase();
+  if (baggage.includes('knife')) {
+    console.log('u are not allowed');
+  } else {
+    console.log('u are wellcome');
+  }
+};
+checkBaggage('i have laptop, some food...');
+checkBaggage('watter');
+checkBaggage('socks');
+
+//working with string 3
+console.log('a+very+nice+string'.split('+'));
+
+const [firstName, lastname] = 'Amin Nicevic'.split(' '); //destructuring
+
+const newName = ['Mr.', firstName, lastname.toUpperCase()].join(' ');
+console.log(newName);
+
+const capitalizedName = function (name) {
+  const names = name.split(' ');
+  const namesUpper = [];
+  for (const n of names) {
+    n[0].toUpperCase() + n.slice[1];
+  }
+  console.log(namesUpper.join(' '));
+};
+capitalizedName('jesica ann smith davis');
+capitalizedName('amin nicevic');
+
+//padding .padStart
+const massage = 'go to gate 23';
+console.log(massage.padStart(25, '+'));
+//ovo je metoda koja nam dodaje onaj karakter koji kazemo
+//dok ne dobijemo onoliku duzinu stringa koju smo trazili
+
+console.log(massage.padEnd(25, '+'));
+//sve istpo samo dodaje posle naseg stringa
+
+const maskCreditCard = function (number) {
+  const str = number + '';
+  const last = str.slice(-4);
+  return last.padStart(str.length, '*');
+};
+
+console.log(maskCreditCard(21321312312313));
+console.log(maskCreditCard('21321312312313'));
+
+//repeat
+const message2 = 'bad weather... all departures delayed...';
+console.log(message2.repeat(5)); //ponovio ih 5 puta
+
+const planesInLine = function (n) {
+  console.log(`there are ${n} planes in line ${'avionce'.repeat(n)}`);
+};
+planesInLine(5);
+planesInLine(3);
+planesInLine(12);
+
+Write a program that receives a list of variable names written in underscore_case 
+and convert them to camelCase.
+The input will come from a textarea inserted into the DOM (see code below to 
+insert the elements), and conversion will happen when the button is pressed.
+Test data (pasted to textarea, including spaces):
+underscore_case
+first_name
+Some_Variable 
+ calculate_AGE
+delayed_departure
+Should produce this output (5 separate console.log outputs):
+underscoreCase ✅
+firstName ✅✅
+someVariable ✅✅✅
+calculateAge ✅✅✅✅
+delayedDeparture ✅✅✅✅✅
+Hints:
+§ Remember which character defines a new line in the textarea �
+§ The solution only needs to work for a variable made out of 2 words, like a_b
+§ Start without worrying about the ✅. Tackle that only after you have the variable 
+name conversion working �
+§ This challenge is difficult on purpose, so start watching the solution in case 
+you're stuck. Then pause and continue!
+Afterwards, test with your own test data!
+GOOD LUCK �
+*/
+
+document.body.append(document.createElement('textarea'));
+document.body.append(document.createElement('button'));
