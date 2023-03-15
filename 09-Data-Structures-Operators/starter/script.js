1f0083457c565379adb8d8a480a44e1bf033c33f
aminnicevic04 <aminnicevic@gmail.com>
2023-03-15 15:30:46 +0100

    From Zero to Expert

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1070,6 +1070,8 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
+#4 CODING CHALLENGE
+
 Write a program that receives a list of variable names written in underscore_case 
 and convert them to camelCase.
 The input will come from a textarea inserted into the DOM (see code below to 
@@ -1097,5 +1099,38 @@ Afterwards, test with your own test data!
 GOOD LUCK �
 */
 
-document.body.append(document.createElement('textarea'));
-document.body.append(document.createElement('button'));
+// document.body.append(document.createElement('textarea'));
+// document.body.append(document.createElement('button'));
+
+// document.querySelector('button').addEventListener('click', function () {
+//   const text = document.querySelector('textarea').value;
+//   const rows = text.split('\n');
+//   console.log(rows);
+
+//   for (const [i, row] of rows) {
+//     const [first, second] = row.toLowerCase().trim().split('_');
+//     const output = `${first}${second.replace(
+//       second[0],
+//       second[0].toUpperCase()
+//     )}`;
+//     console.log(`${output.padEnd[(20, ' ')]}${'✅'.repeat(i + 1)}`);
+//   }
+// });
+
+// how to work with strings
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const output = `${
+    type.startsWith('_Delayed') ? ' ALERT! ' : ''
+  } ${type.replaceAll('_', ' ')} ${getCode(to)} ${to}(${time.replace(
+    ':',
+    'h'
+  )})`.padStart(50);
+
+  console.log(output);
+}
